0fc76c23b494409afbd31ecf8a047409e54a944b
Roman Frołow <rofrol@gmail.com>
2013-04-28 15:41:28 +0200

    works

--- parse.go
+++ parse.go
@@ -1,16 +1,25 @@
 package main
 
 import (
-	"exp/html"
+	"code.google.com/p/go.net/html"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"rofrol/helper"
-	"bytes"
 )
 
-var table *html.Node
 func main() {
+	// bytes.Buffer does not implement io.Writer
+	// output needs to be pointer 
+	// http://my.safaribooksonline.com/book/programming/9780132918961/a-go-primer/ch02lev1sec8
+	// https://groups.google.com/forum/?fromgroups=#!topic/golang-nuts/xXFpT8oLGNU
+	// https://groups.google.com/forum/?fromgroups=#!topic/golang-nuts/jDcbmzDaLi8
+	//var output bytes.Buffer
+	//than html.Render(&ouput, node)
+	//or
+	//output := new(bytes.Buffer)
+	//than html.Render(output, node)
 	var page *os.File
 	var err error
 	if page, err = os.Open("bank_2012.html"); err != nil {
@@ -23,62 +32,36 @@ func main() {
 		log.Fatal(err)
 	}
 
-	var f func(*html.Node)
-	f = func(n *html.Node) {
-		if n.Type == html.ElementNode && n.Data == "table" {
-			for _, a := range n.Attr {
-				if a.Key == "class" {
-					fmt.Println(a.Val)
-					t := helper.FoundClass(a.Val, "contentRoles6")
-					fmt.Println("found class?", t)
-					if t {
-						table = n
-					}
-					break
-				}
+	table := helper.FindByClass(doc, "table", "content")
+	if table != nil {
+		tbody := helper.FirstChildByTag(table, "tbody")
+		trArr := helper.ElementsByTag(tbody, "tr")
+
+		var messages []helper.Message
+		for _, tr := range trArr {
+			tdArr := helper.ElementsByTag(tr, "td")
+			messages = append(messages, helper.String2Message(tdArr))
+		}
+		//fmt.Printf("%+v", messages)
+		filename := "test.csv"
+
+		fmt.Println("writing: " + filename)
+		f, err := os.Create(filename)
+		if err != nil {
+			fmt.Println(err)
+		}
+		for _, m := range messages {
+			// https://groups.google.com/d/msg/golang-nuts/ybwJH4pR0lo/gw38xJgY3AoJ
+			// "%.3f" rounds at the third place after the suffix and truncates to two places after the suffix.
+			//If the suffix would be all zero, it is completely discarded. 
+			//0.5 is rounded towards zero. 
+			s := fmt.Sprintf("%s,%s,%s,%.2f,%.2f\n", m.Title, m.TOrd.Format("2006-01-02"), m.TExe.Format("2006-01-02"), m.Balance, m.Saldo)
+			n, err := io.WriteString(f, s)
+			if err != nil {
+				fmt.Println(n, err)
 			}
 		}
-		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			f(c)
-		}
+		f.Close()
+	}
 
-	child = div.FirstChild
-	html.Render(output, child)
-
-/*
-	for child = child.NextSibling; child == nil; child = child.NextSibling {}
-	html.Render(output, child)
-
-	for child = child.NextSibling; child == nil; child = child.NextSibling {}
-	html.Render(output, child)
-*/
-
-	accounts_history_1 := string(output.Bytes())
-	output.Reset()
-
-	td = td.NextSibling
-	html.Render(output, td.FirstChild)
-	accounts_history_2 := string(output.Bytes())
-	output.Reset()
-
-	td = td.NextSibling
-	html.Render(output, td.FirstChild)
-	accounts_history_3 := string(output.Bytes())
-	output.Reset()
-
-	td = td.NextSibling
-	html.Render(output, td.FirstChild)
-	accounts_history_4 := string(output.Bytes())
-	output.Reset()
-
-	td = td.NextSibling
-	html.Render(output, td.FirstChild)
-	accounts_history_5 := string(output.Bytes())
-	output.Reset()
-
-	fmt.Println("1", accounts_history_1)
-	fmt.Println("2", accounts_history_2)
-	fmt.Println("3", accounts_history_3)
-	fmt.Println("4", accounts_history_4)
-	fmt.Println("5", accounts_history_5)
 }
