6bf887fecfaa65e6d852d43c5e2ee8a79ca4eebd
Roman Frołow <rofrol@gmail.com>
2013-04-28 15:40:45 +0200

    another commit

--- parse.go
+++ parse.go
@@ -3,30 +3,32 @@ package main
 import (
 	"exp/html"
 	"fmt"
-	//"strings"
 	"log"
 	"os"
+	"rofrol/helper"
 )
 
 func main() {
 	var page *os.File
 	var err error
-	if page, err = os.Open("input.txt"); err != nil {
+	if page, err = os.Open("bank_2012.html"); err != nil {
 		return
 	}
 	defer page.Close()
+
 	doc, err := html.Parse(page)
-	//s := `<p>Links:</p><ul><li><a href="foo">Foo</a><li><a href="/bar/baz">BarBaz</a></ul>`
-	//doc, err := html.Parse(strings.NewReader(s))
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	var f func(*html.Node)
 	f = func(n *html.Node) {
-		if n.Type == html.ElementNode && n.Data == "a" {
+		if n.Type == html.ElementNode && n.Data == "table" {
 			for _, a := range n.Attr {
-				if a.Key == "href" {
+				if a.Key == "class" {
 					fmt.Println(a.Val)
+					t := helper.FoundClass(a.Val, "contentRoles6")
+					fmt.Println("found class?", t)
 					break
 				}
 			}
